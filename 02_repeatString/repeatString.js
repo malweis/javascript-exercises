bbab03e9e55f91f4942fc76f4794297cd6dd7135
malweis <58945171+malweis@users.noreply.github.com>
2023-05-11 17:25:10 -0400

    opa

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let resultW = "";
+    if(times < 0){
+        return "ERROR"
+    }
+    for (let i = 0; i < times; i++) {
+        resultW = resultW.concat(word) ;
+        
+    }
+    return resultW
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(one, two) {
+    if(!isNaN(one) ){
+
+    }
+    let sum = 0;
+    for (let i = one; i < two; i++) {
+        sum = sum + i;
+        
+    }
+    return sum + two
 
 };
 
