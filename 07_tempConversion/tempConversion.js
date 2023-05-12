f0630f2ecf3f0bd804b21b959b4fbe1dbf9a047d
malweis <58945171+malweis@users.noreply.github.com>
2023-05-12 09:00:36 -0400

    Temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let decimalC = (temp - 32) * 5/9;
+  return parseFloat(decimalC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let decimalF = (temp * 9/5) + 32;
+  return parseFloat(decimalF.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
